ca58fc5278f297d10cb425e43f035f61d4d3dbd0
mjdebm@yahoo.com <a@b.com>
2025-12-25 23:34:59 +0330

    fix(otp): replace deprecated codes

--- lib/import_export_screen.dart
+++ lib/import_export_screen.dart
@@ -172,7 +172,7 @@ class _ImportExportScreenState extends State<ImportExportScreen> {
           ),
           if (_loading)
             Container(
-              color: Colors.black.withOpacity(0.3),
+              color: Colors.black.withValues(alpha: 0.3),
               child: const Center(child: CircularProgressIndicator()),
             ),
         ],
